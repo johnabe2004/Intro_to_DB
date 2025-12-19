a14f2ea3286aad59fee6d6102bc7d47ebff46c08
Yohannes Abebe <johnabe2004@gmail.com>
2025-12-20 00:37:46 +0300

    multiple records

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,8 @@
-USE alx_book_store;
+-- Use the database provided as an argument
+-- This script inserts multiple records into the 'customer' table
+
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
